a709b0387a43e99f2c91696072f358d3cde57e9f
Max <v.mxvlasov@gmail.com>
2021-12-06 02:15:56 +0300

    call docker build with parameter

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mrmxvlasov/demo-jma-app .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mrmxvlasov/demo-jma-app'
-    }
+    echo "building the application for branch $BRANCH_NAME"
+    sh 'mvn package'
+
 }
